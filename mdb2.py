128baf9ccd21093478b7ff81174899e4a98ff4f6
Leak Testing Machine #2 <administracion@borbolla-automation.com>
2018-09-10 12:37:31 -0500

    SQL Query working on LEak Testing Machine #2

--- mdb2.py
+++ mdb2.py
@@ -1,7 +1,7 @@
 import csv, pyodbc
 
 # set up some constants
-MDB = 'C:\LeakDataManager\DataDB.mdb'; DRV = '{Microsoft Access Driver (*.mdb)}';
+MDB = 'D:\DataManager\DataDB.mdb'; DRV = '{Microsoft Access Driver (*.mdb)}';
 
 
 # connect to db
@@ -9,7 +9,9 @@ con = pyodbc.connect('DRIVER={};DBQ={}'.format(DRV,MDB))
 cur = con.cursor()
 
 # run a query and get the results
-SQL = 'SELECT * FROM mytable;' # your query goes here
+SQL = 'SELECT * FROM LeakTesterData;' # your query goes here
+
 rows = cur.execute(SQL).fetchall()
+print(rows)
 cur.close()
 con.close()
